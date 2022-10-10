d426dd6c9f4055b4dd48178366ce648a34978006
SangBeom-Hahn <hsb990917@gmail.com>
2022-10-10 13:12:31 +0900

    dlinked

---
+++ DataStructure/python/linkedlist/dlinkedlist/header_file.py
@@ -0,0 +1,112 @@
+'''
+tail이 있고 없고의 차이를 생각해보자
+tail이 있으면 첫 노드에 삽입할 때 마지막 노드가 쉽게 삽입할 첫 노드를 바라보게 할 수 있다. 하지만 메모리를 먹는다.
+tail이 없으면 메모리는 안 먹는다 하지만 마지막 노드를 찾기 위해 while을 돌려서
+맨 앞을 보는 노드(마지막 노드가 맨 앞을 보므로)를 찾아야한다.
+
+-> 내 생각에는 연결 리스트의 장점이 삽입, 삭제가 쉽지만 단점이 찾는데 오래걸릴 수 도 있다 였으니
+tail이 있는게 좋을 것 같다.
+'''
+
+
+class Node():
+    def __init__(self, data):
+        self.data = data
+        # 마지막은 일단 None -> 이것으로 마지막 노드에 삽입할 때 ok
+        self.llink = None
+        self.rlink = None
+
+class DLinkedList():
+    def __init__(self, head):
+        self.head = head
+        self.tail = head
+    
+    # 마지막 노드에 삽입
+    def addToEnd(self, node):
+        if(self.head is None):
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            node.next = self.head
+            self.tail = node
+    
+    # 첫 노드에 삽입
+    def addToStart(self, node):
+        if (self.head is None):
+            self.head = node
+            self.tail = node
+        else:
+            node.next = self.head
+            self.head = node
+            self.tail.next = node
+
+    # 중간에 삽입 -> 이게 이중 연결 리스트라서 가능한 것
+    def insertMiddle(self, pre, node):
+        if(self.head is None):
+            self.head = node
+            self.tail = node
+        else:
+            node.rlink = pre.rlink
+            pre.rlink = node
+            node.llink = pre
+            pre.rlink.llink = node
+
+    # 마지막 노드 삭제
+    def deleteEnd(self):
+        tmp = self.head
+        data = ""
+        while(tmp.next is not self.head):
+            # 근데 마지막 노드를 찾는게 문제가 아니고 마지막 노드 하나 전 노드를 찾아야 한다.
+            if(tmp.next.next is self.head):
+                data = tmp.next.data
+                tmp.next = self.head
+                self.tail = tmp
+            else:
+                tmp = tmp.next
+        return data
+
+    # 맨 앞 노드 삭제
+    def deleteMiddle(self, old):
+        data = self.old.data
+        if(self.head.next is None):
+            return
+        else:
+            old.llink.rlink = old.rlink
+            old.rlink.llink = old.llink
+        return data
+
+
+
+
+    # 원하는 값의 데이터 접근
+    def searchData(self, data):
+        tmp = self.head
+        while(tmp):
+            if(tmp.data == data):
+                return tmp # 그 노드 자체를 리턴하는게 포인트
+            else:
+                tmp = tmp.next
+        return -1
+
+    #원하는 인덱스의 데이터 접근
+    def searchIndex(self, index):
+        tmp = self.head
+        for _ in range(index):
+            tmp = tmp.next
+
+        return tmp # 그 노드 자체를 리턴하는게 포인트
+
+
+    # null이 없다 보니 무한 루프가 출력되면 잘 연결한 거다. 하지만 print()를 수정해야한다.
+    # print 해보니 진짜 원형이 느껴진다.
+    def print2(self):
+        tmp = self.head
+        while (1):
+            print(tmp.data,"-> ", end="")
+            tmp = tmp.next
+            if(tmp is self.head):
+                print()
+                break
+
+
---
+++ DataStructure/python/linkedlist/dlinkedlist/src.py
@@ -0,0 +1,29 @@
+from header_file import *
+
+'''
+아니 이런게 어떻게 되지??
+다 ~ 객체 지향 언어이기 때문이지
+속성, 메소드 다 클래스에 쓰고 협력도 매개변수로 클래스를 전달하여 클래스에 쓴다.
+실행은 메인에서 클래스를 객체로 생성해서한다.
+
+↓ ll이라는 객체가 만들어져서 독립된 ll 하나에만 더하고 출력하고 하는 것!!
+'''
+
+ll = DLinkedList(Node("상범"))
+ll.addToEnd(Node("미르"))
+ll.addToStart(Node("한영"))
+
+ll.print2()
+
+ll.deleteEnd()
+n = ll.deleteStart()
+ll.print2()
+print(n)
+
+
+
+
+
+# if(__name__ == '__main__'):
+
+
