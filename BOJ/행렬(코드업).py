b13b20fa7a38b870ce39239e34b7587601dd21fb
SangBeom-Hahn <hsb990917@gmail.com>
2022-10-13 10:37:57 +0900

    matrix

---
+++ BOJ/행렬(코드업).py
@@ -0,0 +1,18 @@
+n = int(input())
+arrMatrix = []
+for i in range(n):
+    arrMatrix.append(input().split())
+
+arrMatrix.sort()
+
+for i in range(1, 20):
+    if(i > int(arrMatrix[len(arrMatrix)-1][0])): #넘으면 패스
+        print(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        continue
+
+    for j in range(1, 20):
+        if(int(arrMatrix[i-1][1]) == j):
+            print(1, end=" ")
+        else:
+            print(0, end=" ")
+    print()
