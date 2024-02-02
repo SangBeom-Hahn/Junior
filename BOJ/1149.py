f0fd8504239c9a2c882b310a6022ab764f2171ae
SangBeom-Hahn <hsb990917@gmail.com>
2024-02-02 10:52:53 +0900

    1149

--- BOJ/1149.py
+++ BOJ/1149.py
@@ -1,25 +1,45 @@
+'''
+n by n 크기의 정사각형이 있다. 상하좌우로 이동할 수 이씅며, 시작좌표는 1, 1이다. 여행가가 이동할
+계획서가 있다. 공간을 벗어나는 움직임은 무시된다. 
+
+1. 모경수
+1) 시작 위치에서 계획서대로 이동함
+2) 공간을 벗어나면 무시하고 다음 계획을 봄
+3) 모든 계획으로 이동하면 끝
+
+* n : 공간의 크기
+계획서 
+
+출력 : 결과 좌표
+'''
+
 n = int(input())
-dp = [[0] * n for i in range(3)]
-arr = [[0] * n for i in range(3)]
+plans = input().split()
 
-# print(arr)
+# print(plan)
 
-for j in range(n):
-    l = list(map(int, input().split()))
-    for i in range(3):
-        arr[i][j] = l[i]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+d_word = ['U', 'D', 'L', 'R']
 
-for i in range(3):
-    dp[i][0] = arr[i][0]
+'''
+1) 시작 위치에서 계획서대로 이동함
+2) 공간을 벗어나면 무시하고 다음 계획을 봄
+3) 모든 계획으로 이동하면 끝
+'''
 
-for j in range(1, n):
-    for i in range(3):
-        dp[i][j] = min(dp[(i+1) % 3][j-1], dp[(i+2) % 3][j-1]) + arr[i][j]
-
-mini = 99999999
-for i in range(3):
-    if(mini > dp[i][n-1]):
-        mini = dp[i][n-1]
-
-print(mini)
-# print(dp)
+x, y = 0, 0
+for plan in plans:
+    # 계획서의 글자와 방향을 매칭해야함
+    d_idx = d_word.index(plan)
+    
+    nx = x + dx[d_idx]
+    ny = y + dy[d_idx]
+    
+    # 무시
+    if(nx < 0 or ny < 0 or nx >= n or ny >= n):
+        continue
+    x = nx
+    y = ny
+    
+print(x+1, y+1)
