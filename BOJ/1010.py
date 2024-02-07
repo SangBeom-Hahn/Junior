bbc75948aea61d40002675bbc09ef895a21afb46
SangBeom-Hahn <hsb990917@gmail.com>
2024-02-07 20:57:28 +0900

    1010

--- BOJ/1010.py
+++ BOJ/1010.py
@@ -1,10 +1,42 @@
-import math
+'''
 
-n = int(input())
-sight = []
+'''
 
-for i in range(n):
-    sight.append(list(map(int, input().split())))
+def check(mid, s, n):
+    cnt = 1
+    max_freq = 0
+    freq = [0] * 26
+    
+    for i in range(len(s)):
+        freq[ord(s[i]) - ord('a')] += 1
+        max_freq = max(max_freq, freq[ord(s[i]) - ord('a')])
 
-for west, east in sight:
-    print(math.comb(east, west))
+        if max_freq * cnt > mid:
+            max_freq = freq[ord(s[i]) - ord('a')]
+            cnt += 1
+            
+    return cnt <= n
+
+def solution(s, n):
+    left = 1
+    right = len(s) * 26
+    answer = 0
+    
+    while left <= right:
+        mid = (left + right) // 2
+        if check(mid, s, n):
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+            
+    return answer // n
+
+
+# "aabbbabba"
+# "xyyyxxxxxx"
+
+s = "abcd"
+n = 1
+
+print(solution(s, n))
