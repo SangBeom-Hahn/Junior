c74ed2f4a5a9c3e4d8d5481e9366dc7fcf50e0ac
SangBeom-Hahn <hsb990917@gmail.com>
2024-02-12 10:09:45 +0900

    1122

--- BOJ/1122.py
+++ BOJ/1122.py
@@ -1,18 +1,19 @@
-def estimate_auroc(X, Y):
-    auroc = 0.0  # Initialize the AUROC value
-    
-    for i in range(len(X) - 1):
-        # Calculate the trapezoidal area between two consecutive points
-        area = (X[i+1] - X[i]) * (Y[i] + Y[i+1]) / 2.0
-        
-        # Add the area to the AUROC value
-        auroc += area
-    
-    return auroc
+'''
 
+'''
 
-# Example usage
-x = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-y = [0.0, 0.2, 0.4, 0.5, 0.6, 0.7, 0.75, 0.8, 0.9, 0.95, 1.0]
-result = estimate_auroc(x, y)
-print(result)
+arr = [0, 7, 3, 5, 8, 2, 1, 4, 6, 9]
+
+def q(arr):
+    if(len(arr) <= 1):
+        return arr
+    
+    p = arr[0]
+    tail = arr[1:]
+    
+    left = [i for i in tail if(i < p)]
+    right = [i for i in tail if(i >= p)]
+    
+    return q(left) + [p] + q(right)
+
+print(q(arr))
