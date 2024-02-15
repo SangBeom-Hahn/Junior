e6b96b5bc93474deb6183dc905d71edfe5459bee
SangBeom-Hahn <hsb990917@gmail.com>
2024-02-15 20:49:47 +0900

    1149

--- BOJ/1149.py
+++ BOJ/1149.py
@@ -1,45 +1,51 @@
 '''
-n by n 크기의 정사각형이 있다. 상하좌우로 이동할 수 이씅며, 시작좌표는 1, 1이다. 여행가가 이동할
-계획서가 있다. 공간을 벗어나는 움직임은 무시된다. 
+x에 할 수 있는 연산은 다음과 같다.
+1) 3으로 나눠 떨어지면 3으로 나눈다.
+2) 2로 나눠 떨어지면 2로 나눈다
+3) 1을 뺀다.
+
+정수 n이 있을 때 연산 3개를 적절히 사용해서 1을 만드려고 한다. 
 
 1. 모경수
-1) 시작 위치에서 계획서대로 이동함
-2) 공간을 벗어나면 무시하고 다음 계획을 봄
-3) 모든 계획으로 이동하면 끝
+1) 결과값
+2) 점화식
+3) base
+    1] 0 dp 배열, -1 저장소 배열
+    2] 2부터 시작
 
-* n : 공간의 크기
-계획서 
+* n
+출력 : 연산을 사용하는 횟수의 최소값, 1f로 만드는 방법에 포함되어 있느 수
 
-출력 : 결과 좌표
+2. 시복 : nlogn
 '''
 
 n = int(input())
-plans = input().split()
+dp = [0] * (10**6+1)
+store = [-1]* (10**6+1)
 
-# print(plan)
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-d_word = ['U', 'D', 'L', 'R']
-
-'''
-1) 시작 위치에서 계획서대로 이동함
-2) 공간을 벗어나면 무시하고 다음 계획을 봄
-3) 모든 계획으로 이동하면 끝
-'''
-
-x, y = 0, 0
-for plan in plans:
-    # 계획서의 글자와 방향을 매칭해야함
-    d_idx = d_word.index(plan)
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + 1
+    store[i] = i-1
     
-    nx = x + dx[d_idx]
-    ny = y + dy[d_idx]
+    if(i % 3 == 0):
+        if(dp[i] > dp[i//3]+1):
+            dp[i] = min(dp[i], dp[i//3] + 1)
+            store[i] = i//3
+        
+    if(i % 2 == 0):
+        if(dp[i] > dp[i//2]+1):
+            dp[i] = min(dp[i], dp[i//2] + 1)
+            store[i] = i//2
+        
+# print(dp[:11])
+# print(store[:11])
+
+print(dp[n])
+
+want = n
+while(True):
+    print(want, end = ' ')
+    want = store[want]
     
-    # 무시
-    if(nx < 0 or ny < 0 or nx >= n or ny >= n):
-        continue
-    x = nx
-    y = ny
-    
-print(x+1, y+1)
+    if(want == -1):
+        break
