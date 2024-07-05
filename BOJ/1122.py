690b7196091a8df26ae28fab1e95a7081761a471
SangBeom-Hahn <hsb990917@gmail.com>
2024-07-05 09:44:30 +0900

    1122

--- BOJ/1122.py
+++ BOJ/1122.py
@@ -1,52 +1,25 @@
 '''
-마음을 읽는 기계, 각 사람들이 누구를 찍을지 미리 읽을 수 있어 국회 선거 조작할거다.
-어떤 사람이 누구를 찍을지 정하면 반드시 선거때 그 사람을 찍음
-후보 n명, 다솜이는 기호1번, 마을 주민 m명, m명의 마음을 읽어서 자신을 찍지 않은 사람을
-돈으로 매수해서 조작할거임
-다른 모든 사람의 득표수보다 많은 득표수를 가질때 당선됨
-다솜이가 매수해야 하는 사람의 최소값을 ㄱ
+2차원 배열 z 모양 탐색한다. 2x2 보다 큰 4x4, 8x8의 경우 4등분한 후 재귀적으로 
+순서대로 방문한다. r행, c열을 몇 번째로 방문하는지 구해라
 
-ex) 기호 1  2   3
-    5       7   7
-    7(5+1+1)       6(7-1)   6(7-1)
+1. 모경수(prt, n=1)
+1) 4등분으로 재귀
+2) mid를 방문 순서로 활용
+3) 재귀하면 좌상단으로 모는 기술
 
-1. 모경수(prt, n=1)    
-1) 백으로 탐색, 그냥 배열 훑기 유형
-    
-* n : 후보의 수
-기호 1~n번을 찍으려는 사람의 수
-
-2. 시복 : n^3
+* n, r, c : 2^n x 2^n (n=1이면 2x2), r행 c열
+출 : r행 c열을 몇 번째로 방문하는지
+2.시복 : 4^15
 '''
 
-def back(candis, cnt): # 매수자 수
-    global min_cnt
-    # print(candis)
-    me = candis[0]
-    for i in range(1, len(candis)):
-        if(me <= candis[i]): # 작거나 같으면 다시 재귀 ㄱ
-            break
-    else: # 전부 다 크면
-        if(cnt != 0 and min_cnt > cnt):
-            min_cnt = cnt # 현재가 최소값이라 추가 재귀 안함
-            return 
+def rec(n, r, c):
+    if(n==0): return 0
+    
+    mid = 2**n // 2
+    if(r < mid and c < mid): return rec(n-1, r, c)
+    if(r < mid and c >= mid): return mid * mid + rec(n-1, r, c-mid)
+    if(r >= mid and c < mid): return 2* mid * mid + rec(n-1, r-mid, c)
+    return 3 * mid * mid + rec(n-1, r-mid, c-mid)
 
-    for i in range(1, len(candis)):
-        if(candis[i] > 0): # 한 개라도 있다면
-            candis[0] += 1
-            candis[i] -= 1
-            back(candis, cnt+1)
-            candis[0] -= 1
-            candis[i] += 1
-            # print(i, candis)
-
-min_cnt = 50000
-n = int(input())
-candis = [int(input()) for _ in range(n)]
-# print(candis)
-
-if(n == 1):
-    print(0)
-else:
-    back(candis, 0)
-    print(min_cnt)
+n, r, c = map(int, input().split())
+print(rec(n, r, c))
